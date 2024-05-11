ca2eb73977b3299dba5bd22b36bd22ff1da8fec1
21paradox <1036339815@qq.com>
2024-05-11 16:54:16 +0800

    feat: usbtty boot. allow to use usbtty cable for installation, cable baudrate should set to 9600 (maybe ch340/341 driver issue)

--- nixos/modules/installer/cd-dvd/iso-image.nix
+++ nixos/modules/installer/cd-dvd/iso-image.nix
@@ -179,7 +179,7 @@ let
     # Search using a "marker file"
     search --set=root --file /EFI/nixos-installer-image
 
-    insmod gfxterm
+    #insmod gfxterm
     insmod png
     set gfxpayload=keep
     set gfxmode=${concatStringsSep "," [
@@ -321,8 +321,9 @@ let
 
     cat <<EOF > $out/EFI/boot/grub.cfg
 
-    set textmode=${boolToString (config.isoImage.forceTextMode)}
+    set textmode=true
     set timeout=${toString grubEfiTimeout}
+    set GRUB_DEFAULT="HiDPI, Quirks and Accessibility>Serial console=ttyS0,115200n8"
 
     clear
     # This message will only be viewable on the default (UEFI) console.
@@ -404,7 +405,10 @@ let
       submenu "" {return}
       submenu "Serial console=ttyS0,115200n8" --class serial {
         ${grubMenuCfg}
-        ${buildMenuAdditionalParamsGrub2 "console=ttyS0,115200n8"}
+        serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+        terminal_input --append serial
+        terminal_output --append serial
+        ${buildMenuAdditionalParamsGrub2 "console=ttyUSB0,115200n8 console=tty1"}
       }
     }
 
